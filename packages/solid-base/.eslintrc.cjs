31eb74a6f5ef95ffb1e4712a70ae5aeb17b1882d
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2022-11-14 01:23:07 +0900

    use code-config@test

--- packages/solid-base/.eslintrc.cjs
+++ packages/solid-base/.eslintrc.cjs
@@ -1,5 +1,5 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   root: true,
-  extends: ['@suzulabo/eslint-config/solidjs'],
+  extends: ['@suzulabo/eslint-config/solid-js'],
 };
