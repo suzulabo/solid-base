298e5225d8ad35187c4860e30b0567855565429c
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2022-11-20 23:26:11 +0900

    rename files

--- packages/solid-base/.storybook/preview.js
+++ packages/solid-base/.storybook/preview.js
@@ -1,5 +1,5 @@
-import BaseStyles from '../src/components/BaseStyle';
-import Theme from '../src/components/Theme';
+import BaseStyles from '../src/components/BaseStyle/BaseStyle';
+import Theme from '../src/components/Theme/Theme';
 
 export const decorators = [
   (Story) => {
