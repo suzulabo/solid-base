6b977c95ea5f032b9bc29afdd15526dfce101925
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2022-12-11 19:06:51 +0900

    set dark mode class in preview

--- packages/solid-base/.storybook/preview.js
+++ packages/solid-base/.storybook/preview.js
@@ -1,16 +1,7 @@
-import addons from '@storybook/addons';
-import { DARK_MODE_EVENT_NAME } from 'storybook-dark-mode';
 import BaseStyle from '../src/components/BaseStyle/BaseStyle';
-import { setThemeMode } from '../src/components/Theme/theme';
-
-const channel = addons.getChannel();
 
 export const decorators = [
   (Story) => {
-    channel.on(DARK_MODE_EVENT_NAME, (darkMode) => {
-      setThemeMode(darkMode ? 'dark' : 'light');
-    });
-
     return (
       <div>
         <BaseStyle />
@@ -19,3 +10,9 @@ export const decorators = [
     );
   },
 ];
+
+export const parameters = {
+  darkMode: {
+    stylePreview: true,
+  },
+};
