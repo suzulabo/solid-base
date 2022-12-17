dec029cbe7d8454657a513ebc440a50e722022e9
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2022-12-17 23:52:12 +0900

    use typescript-plugin-css-modules

--- packages/solid-base/src/components/Button/Button.tsx
+++ packages/solid-base/src/components/Button/Button.tsx
@@ -1,9 +1,13 @@
-import './Button.css';
+import styles from './Button.module.css';
 
 import type { ParentComponent } from 'solid-js';
 
 const Button: ParentComponent = (props) => {
-  return <button {...props}>{props.children}</button>;
+  return (
+    <button {...props} class={styles.Button}>
+      {props.children}
+    </button>
+  );
 };
 
 export default Button;
