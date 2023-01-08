905899ac44f0b39c7548cd1d023c506aaa0cd0aa
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2023-01-08 19:54:19 +0900

    Update storybook

--- packages/solid-base/.storybook/main.js
+++ packages/solid-base/.storybook/main.js
@@ -1,14 +1,8 @@
 module.exports = {
   stories: ['../src/components/**/*.stories.tsx'],
   addons: ['storybook-dark-mode'],
-
-  /*
-  It won't work
-  Use .babelrc
-
-  babel: async (options) => ({
-    ...options,
-    presets: ['solid'],
-  }),
-  */
+  framework: {
+    name: '@storybook/html-vite',
+    options: {},
+  },
 };
