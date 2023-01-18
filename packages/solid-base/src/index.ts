2a2be238a4c90d4cb77aea3c534127bc929ff8dc
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2023-01-18 22:26:43 +0900

    Add DotPulse

--- packages/solid-base/src/index.ts
+++ packages/solid-base/src/index.ts
@@ -1,3 +1,4 @@
 export { default as BaseStyle } from './components/BaseStyle/BaseStyle';
 export { default as Blink } from './components/Blink/Blink';
 export { default as Button } from './components/Button/Button';
+export { default as DotPulse } from './components/DotPulse/DotPulse';
